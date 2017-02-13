666a6eb909c0d7df30a229e288d1a03e785d4055
Jesse Scott <jesse.scott@eventbase.com>
2017-02-12 21:09:06 -0800

    fixed module madness with local aar's

--- CardboardVideoPlayer/app/src/main/java/ca/ecuad/research/s3dcentre/cardboardvideoplayer/MainActivity.java
+++ CardboardVideoPlayer/app/src/main/java/ca/ecuad/research/s3dcentre/cardboardvideoplayer/MainActivity.java
@@ -1,13 +1,201 @@
 package ca.ecuad.research.s3dcentre.cardboardvideoplayer;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import com.google.vrtoolkit.cardboard.widgets.video.VrVideoEventListener;
+import com.google.vrtoolkit.cardboard.widgets.video.VrVideoView;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final String STATE_IS_PAUSED = "isPaused";
+    private static final String STATE_PROGRESS_TIME = "progressTime";
+    private static final String STATE_VIDEO_DURATION = "videoDuration";
+    private boolean loadVideoSuccessful = false;
+    public boolean isLoadVideoSuccessful() {
+        return loadVideoSuccessful;
+    }
+    private Uri fileUri;
+    private VideoLoaderTask backgroundVideoLoaderTask;
+    private VrVideoView videoWidgetView;
+    private boolean isPaused = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
+
+        // Bind input and output objects for the view.
+        videoWidgetView = (VrVideoView) findViewById(R.id.video_view);
+        videoWidgetView.setEventListener(new ActivityEventListener());
+
+        // Initial launch of the app or an Activity recreation due to rotation.
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Log.i(TAG, this.hashCode() + ".onNewIntent()");
+        // Save the intent. This allows the getIntent() call in onCreate() to use this new Intent during
+        // future invocations.
+        setIntent(intent);
+        // Load the new image.
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        // Determine if the Intent contains a file to load.
+        if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            Log.i(TAG, "ACTION_VIEW Intent received");
+
+            fileUri = intent.getData();
+            if (fileUri == null) {
+                Log.w(TAG, "No data uri specified. Use \"-d /path/filename\".");
+            } else {
+                Log.i(TAG, "Using file " + fileUri.toString());
+            }
+        } else {
+            Log.i(TAG, "Intent is not ACTION_VIEW. Using the default video.");
+            fileUri = null;
+        }
+
+        // Load the bitmap in a background thread to avoid blocking the UI thread. This operation can
+        // take 100s of milliseconds.
+        if (backgroundVideoLoaderTask != null) {
+            // Cancel any task from a previous intent sent to this activity.
+            backgroundVideoLoaderTask.cancel(true);
+        }
+        backgroundVideoLoaderTask = new VideoLoaderTask();
+        backgroundVideoLoaderTask.execute(fileUri);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putLong(STATE_PROGRESS_TIME, videoWidgetView.getCurrentPosition());
+        savedInstanceState.putLong(STATE_VIDEO_DURATION, videoWidgetView.getDuration());
+        savedInstanceState.putBoolean(STATE_IS_PAUSED, isPaused);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        long progressTime = savedInstanceState.getLong(STATE_PROGRESS_TIME);
+        videoWidgetView.seekTo(progressTime);
+
+        isPaused = savedInstanceState.getBoolean(STATE_IS_PAUSED);
+        if (isPaused) {
+            videoWidgetView.pauseVideo();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Prevent the view from rendering continuously when in the background.
+        videoWidgetView.pauseRendering();
+        // If the video was playing when onPause() iss called, the default behavior will be to pause
+        // the video and keep it paused when onResume() is called.
+        isPaused = true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Resume the 3D rendering.
+        videoWidgetView.resumeRendering();
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Destroy the widget and free memory.
+        videoWidgetView.shutdown();
+        super.onDestroy();
+    }
+
+    private void togglePause() {
+        if (isPaused) {
+            videoWidgetView.playVideo();
+        } else {
+            videoWidgetView.pauseVideo();
+        }
+        isPaused = !isPaused;
+    }
+
+    /*
+
+     */
+    private class ActivityEventListener extends VrVideoEventListener  {
+
+        @Override
+        public void onLoadSuccess() {
+            Log.i(TAG, "Sucessfully loaded video ");
+            loadVideoSuccessful = true;
+            videoWidgetView.playVideo();
+        }
+
+        @Override
+        public void onLoadError(String errorMessage) {
+            // An error here is normally due to being unable to decode the video format.
+            loadVideoSuccessful = false;
+            Toast.makeText(
+                    MainActivity.this, "Error loading video: " + errorMessage, Toast.LENGTH_LONG)
+                    .show();
+            Log.e(TAG, "Error loading video: " + errorMessage);
+        }
+
+        @Override
+        public void onClick() {
+            togglePause();
+        }
+
+
+        @Override
+        public void onCompletion() {
+            videoWidgetView.seekTo(0);
+        }
+    }
+
+    /*
+
+     */
+    class VideoLoaderTask extends AsyncTask<Uri, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Uri... uri) {
+            try {
+                if (uri == null || uri.length < 1 || uri[0] == null) {
+                    videoWidgetView.loadVideoFromAsset("nkmip.mp4");
+                } else {
+                    videoWidgetView.loadVideo(uri[0]);
+                }
+            } catch (IOException e) {
+                // An error here is normally due to being unable to locate the file.
+                loadVideoSuccessful = false;
+                // Since this is a background thread, we need to switch to the main thread to show a toast.
+                videoWidgetView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast
+                                .makeText(MainActivity.this, "Error opening file. ", Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+                Log.e(TAG, "Could not open video: " + e);
+            }
+
+            return true;
+        }
     }
 }
+
