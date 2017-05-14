a5741458b5da50a5f72e73cccb185682e0655182
Jesse Scott <jessecolinscott@gmail.com>
2017-05-13 22:09:27 -0700

    cleanup

--- CardboardVideoPlayer/app/src/main/java/ca/ecuad/research/s3dcentre/cardboardvideoplayer/nkmip/MainActivity.java
+++ CardboardVideoPlayer/app/src/main/java/ca/ecuad/research/s3dcentre/cardboardvideoplayer/nkmip/MainActivity.java
@@ -1,7 +1,8 @@
 package ca.ecuad.research.s3dcentre.cardboardvideoplayer.nkmip;
 
-import com.google.vrtoolkit.cardboard.widgets.video.VrVideoEventListener;
-import com.google.vrtoolkit.cardboard.widgets.video.VrVideoView;
+import com.google.vr.sdk.widgets.video.VrVideoEventListener;
+import com.google.vr.sdk.widgets.video.VrVideoView;
+import com.google.vr.sdk.widgets.video.VrVideoView.Options;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -9,6 +10,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -22,13 +24,18 @@ public class MainActivity extends Activity {
     private static final String STATE_IS_PAUSED = "isPaused";
     private static final String STATE_PROGRESS_TIME = "progressTime";
     private static final String STATE_VIDEO_DURATION = "videoDuration";
-    private boolean loadVideoSuccessful = false;
-    public boolean isLoadVideoSuccessful() {
-        return loadVideoSuccessful;
-    }
+
+    public static final int LOAD_VIDEO_STATUS_UNKNOWN = 0;
+    public static final int LOAD_VIDEO_STATUS_SUCCESS = 1;
+    public static final int LOAD_VIDEO_STATUS_ERROR = 2;
+    private int loadVideoStatus = LOAD_VIDEO_STATUS_UNKNOWN;
+
     private VideoLoaderTask backgroundVideoLoaderTask;
     private VrVideoView videoWidgetView;
     private boolean isPaused = false;
+    private Options videoOptions = new Options();
+    private Uri fileUri;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,8 @@ public class MainActivity extends Activity {
 
         videoWidgetView = (VrVideoView) findViewById(R.id.video_view);
         videoWidgetView.setEventListener(new ActivityEventListener());
+
+        loadVideoStatus = LOAD_VIDEO_STATUS_UNKNOWN;
 
         handleIntent(getIntent());
     }
@@ -49,27 +58,26 @@ public class MainActivity extends Activity {
     }
 
     private void handleIntent(Intent intent) {
-        // Determine if the Intent contains a file to load.
-        Uri fileUri;
         if (Intent.ACTION_VIEW.equals(intent.getAction())) {
             Log.i(TAG, "ACTION_VIEW Intent received");
-
             fileUri = intent.getData();
             if (fileUri == null) {
                 Log.w(TAG, "No data uri specified. Use \"-d /path/filename\".");
             } else {
                 Log.i(TAG, "Using file " + fileUri.toString());
             }
+
+            videoOptions.inputFormat = intent.getIntExtra("inputFormat", Options.FORMAT_DEFAULT);
+            videoOptions.inputType = intent.getIntExtra("inputType", Options.TYPE_MONO);
         } else {
             Log.i(TAG, "Intent is not ACTION_VIEW. Using the default video.");
             fileUri = null;
         }
-
         if (backgroundVideoLoaderTask != null) {
             backgroundVideoLoaderTask.cancel(true);
         }
         backgroundVideoLoaderTask = new VideoLoaderTask();
-        backgroundVideoLoaderTask.execute(fileUri);
+        backgroundVideoLoaderTask.execute(Pair.create(fileUri, videoOptions));
     }
 
     @Override
@@ -129,13 +137,13 @@ public class MainActivity extends Activity {
         @Override
         public void onLoadSuccess() {
             Log.i(TAG, "Sucessfully loaded video ");
-            loadVideoSuccessful = true;
+            loadVideoStatus = LOAD_VIDEO_STATUS_SUCCESS;
             videoWidgetView.playVideo();
         }
 
         @Override
         public void onLoadError(String errorMessage) {
-            loadVideoSuccessful = false;
+            loadVideoStatus = LOAD_VIDEO_STATUS_ERROR;
             Toast.makeText(MainActivity.this, "Error loading video: " + errorMessage, Toast.LENGTH_LONG).show();
             Log.e(TAG, "Error loading video: " + errorMessage);
         }
@@ -155,19 +163,18 @@ public class MainActivity extends Activity {
     /*
 
      */
-    class VideoLoaderTask extends AsyncTask<Uri, Void, Boolean> {
+    class VideoLoaderTask extends AsyncTask<Pair<Uri, Options>, Void, Boolean> {
         @Override
-        protected Boolean doInBackground(Uri... uri) {
+        protected Boolean doInBackground(Pair<Uri, Options>... fileInformation) {
             try {
-                if (uri == null || uri.length < 1 || uri[0] == null) {
-                    videoWidgetView.loadVideoFromAsset(DEFAULT_ASSET);
+                if (fileInformation == null || fileInformation.length < 1 || fileInformation[0] == null || fileInformation[0].first == null) {
+                    Options options = new Options();
+                    options.inputType = Options.TYPE_STEREO_OVER_UNDER;
+                    videoWidgetView.loadVideoFromAsset(DEFAULT_ASSET, options);
                 } else {
-                    videoWidgetView.loadVideo(uri[0]);
+                    videoWidgetView.loadVideo(fileInformation[0].first, fileInformation[0].second);
                 }
             } catch (IOException e) {
-                // An error here is normally due to being unable to locate the file.
-                loadVideoSuccessful = false;
-                // Since this is a background thread, we need to switch to the main thread to show a toast.
                 videoWidgetView.post(new Runnable() {
                     @Override
                     public void run() {
@@ -180,5 +187,6 @@ public class MainActivity extends Activity {
             return true;
         }
     }
+
 }
 
